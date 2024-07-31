0d87cc2085b94fd58a469da8a8416fbebb0d39a7
Gabriel Brum <grodrigues@Gabriel-Brum.localdomain>
2024-07-30 21:08:43 -0300

    fox

--- src/modules/execution/execution.module.ts
+++ src/modules/execution/execution.module.ts
@@ -4,9 +4,10 @@ import { ExecutionController } from './execution.controller';
 import { ExecutionEntity } from './entities/execution.entity';
 import { ExecutionValueEntity } from './entities/execution-value.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TechnicianEntity } from '@modules/technician/entities/technician.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExecutionEntity, ExecutionValueEntity])],
+  imports: [TypeOrmModule.forFeature([ExecutionEntity, ExecutionValueEntity, TechnicianEntity])],
   controllers: [ExecutionController],
   providers: [ExecutionService],
 })
